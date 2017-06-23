6e1f183914f9f8049fa0549eba36c6b65cae058e
EWomack <womackx@live.com>
2017-06-23 12:50:36 +0100

    commit

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 /**
  * Created by EWomack on 23/06/2017.
  */
+const Discord = require('discord.js');
+
+const bot = new Discord.Client();
+
+bot.login("MzI1NzU2ODg1OTU1Mzc5MjEx.DC6ElQ.Cribu5n8W10heFkGc7D3nq3RBSY");
+
+bot.on('message', (message) => {
+    if (message.content == "Ping") {
+        message.reply("Pong")
+    }
+});
