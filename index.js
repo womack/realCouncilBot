73dc9b747fd8f44985b049652447d420775bf2fe
Elot <womackx@live.com>
2017-06-25 18:02:51 +0100

    smells

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 //file require for auth key since i'm not pushing it to git
 const data = require("./resources/privates.js");
-const publicIp = require('public-ip');
+const publicIp = require("public-ip");
 var fs = require("fs");
-//setting up bot with commando library
 const commando = require("discord.js-commando");
 const bot = new commando.Client();
+
 //random commands, currently only dice
 bot.registry.registerGroup("random", "Random");
 //arakoooone
@@ -15,12 +15,13 @@ bot.registry.registerGroup("admin", "Admin");
 bot.registry.registerDefaults();
 //registering all the commands in the directory
 bot.registry.registerCommandsIn(__dirname + "/commands");
+
 bot.login(data.auth.key);
 console.log("Online");
 
 bot.on("message", (message) => {
     if (message.content === "checkIP" && message.author.username === "womackx") {
-        publicIp.v4().then(ip => {
+        publicIp.v4().then((ip) => {
             message.reply(ip);
         });
     }
