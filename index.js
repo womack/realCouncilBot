1f88426a7ee8ecbb539368e5f857003edbc38e18
EWomack <womackx@live.com>
2017-06-26 13:39:25 +0100

    moved checkIP to commando structure, like it should be.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 //file require for auth key since i'm not pushing it to git
 const data = require("./resources/privates.js");
-const publicIp = require("public-ip");
 var fs = require("fs");
 const commando = require("discord.js-commando");
 const bot = new commando.Client();
@@ -11,21 +10,15 @@ bot.registry.registerGroup("random", "Random");
 bot.registry.registerGroup("quotes", "Quotes");
 //admin functionality
 bot.registry.registerGroup("admin", "Admin");
-//admin functionality
+//roles functionality
 bot.registry.registerGroup("roles", "Roles");
 //default commands, not sure if beneficial
 bot.registry.registerDefaults();
 //registering all the commands in the directory
 bot.registry.registerCommandsIn(__dirname + "/commands");
 
-bot.login(data.auth.key);
+bot.login(data.key);
 console.log("Online");
 
-bot.on("message", (message) => {
-    if (message.content === "checkIP" && message.author.username === "womackx") {
-        publicIp.v4().then((ip) => {
-            message.reply(ip);
-        });
-    }
-});
+
 
