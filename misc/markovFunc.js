275e295390077e677b8b07220336c58e6057fc0f
womackx <womackx@live.com>
2017-07-08 21:21:22 +0100

    file search functionality

--- misc/markovFunc.js
+++ misc/markovFunc.js
@@ -1,25 +1,30 @@
 const Markovchain = require("markovchain");
 const fs = require("fs");
 const starters = ["im", "i", "the", "Im", "The", "we"];
-const notScared = ["womackx", "randler", "bloods", "arakune", "rifletown"];
+const notScared = ["womackx", "randler", "bloods", "arakune", "rifletown", "213213", "hansalel"];
 
-
-let markovQuote = function (name) {
+let startQuote = function (name) {
     let markovQuotes = null;
     try {
         markovQuotes = new Markovchain(fs.readFileSync(`resources/quotes/${name}.txt`, "utf8"));
     }
-    catch (err) { }
-    if (markovQuotes == null || markovQuotes == undefined || !notScared.includes(name.toLowerCase())) {
+    catch (err) {
+        console.log(err);
+    }
+    if (markovQuotes == null || !notScared.includes(name.toLowerCase())) {
         return "Not found, or too scared";
     }
+    return markovQuote(markovQuotes);
+};
+
+let markovQuote = function (markovQuotes) {
     const startNum = Math.floor(Math.random() * starters.length);
     let quote = markovQuotes.start(starters[startNum]).end().process();
     if (quote == null || quote.length > 2000 || quote.length < 15) {
-        quote = markovQuote(name);
+        quote = markovQuote(markovQuotes);
     }
     return quote;
-};
+}
 
 let listMarkovs = function () {
     let reply = "People who aint vulnerable baboos\n";
@@ -27,7 +32,13 @@ let listMarkovs = function () {
     return reply;
 }
 
+let searchFiles = function (name) {
+    return fs.readdirSync("./resources/quotes/").map((a) => a.replace(".txt", "")).find((b) => b.toLowerCase().includes(name.toLowerCase()));
+}
+
 module.exports = {
     markovQuote,
-    listMarkovs
+    listMarkovs,
+    searchFiles,
+    startQuote
 }
